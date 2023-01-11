47da945c8734b1bd757985e8bb57ef7e98d1432e
SSY1203 <tlstjsdud566@gmail.com>
2023-01-11 17:59:56 +0900

    [선영] 마크업 끝

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
-var btn = document.querySelector('.search');
+var btn = document.querySelector('.home-search');
+var searchContainer = document.querySelector('.search-container');
 
 btn.addEventListener('click', function (e) {
   e.preventDefault();
+
   btn.classList.toggle('is--active');
+  searchContainer.classList.toggle('is--active');
+
   if (btn.classList.contains('is--active')) {
-    btn.setAttribute('aria-label', '검색 닫기');
-    // 메뉴 열기 버튼 최초 클릭 시 트랜지션 설정
+    btn.setAttribute('aria-label', '메뉴 닫기');
     menuList.style.cssText = 'transition:all 350ms ease-in-out';
-    keyboardTrapInMenu();
   } else {
-    btn.setAttribute('aria-label', '검색 열기');
-    keyboardUntrapInMenu();
+    btn.setAttribute('aria-label', '메뉴 열기');
   }
-  menu.classList.toggle('is--active');
 });
