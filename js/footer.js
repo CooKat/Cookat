0b204c75ff99aac7ef0f5bb42046c4b1467d717a
semin99 <jsm7494@naver.com>
2023-01-22 00:14:46 +0900

    푸터 오류 수정

--- js/footer.js
+++ js/footer.js
@@ -1,20 +1,23 @@
-var coll = document.getElementsByClassName(
+const coll = document.getElementsByClassName(
   'business-info-title-container'
 );
-var i;
+let i;
+let target = document.querySelector('.business-info-list');
+let h = target.offsetHeight;
+target.style = `display:block; height: ${h}px; transition:height 0.5s`;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function () {
     this.classList.toggle('active');
-
-    var content = this.nextElementSibling; // ul
-    var img = this.lastElementChild; // button img
+    
+    let content = this.nextElementSibling; // ul
+    let img = this.lastElementChild; // button img
 
     if (content.style.display === 'block') {
-      content.style = 'height: 260px; transition:height 0.5s';
+      content.style = `height: 0; transition:height 0.5s`;
       img.style = 'transform: rotate(0deg)';
     } else {
-      content.style = 'display: block; height: 0; transition:height 0.5s';
+      content.style = `display:block; height: ${h}px; transition:height 0.5s`;
       img.style = 'transform: rotate(-180deg); ';
     }
   });
