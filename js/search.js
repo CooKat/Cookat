39479726a74e65f1308c0f18dc0ab3de14596bf3
semin99 <jsm7494@naver.com>
2023-01-22 01:02:22 +0900

    best 페이지 검색 기능 삽입

--- js/search.js
+++ js/search.js
@@ -16,12 +16,12 @@ btn.addEventListener('click', function (e) {
   if (btn.classList.contains('is--active')) {
     btn.setAttribute('aria-label', '메뉴 닫기');
     searchContainer.style.display = 'block';
-    fixedAdvertiseContainer.style.display = 'none';
+    // fixedAdvertiseContainer.style.display = 'none';
     body.style.overflowY = 'hidden';
   } else {
     btn.setAttribute('aria-label', '메뉴 열기');
     searchContainer.style.display = 'none';
-    fixedAdvertiseContainer.style.display = 'block';
+    // fixedAdvertiseContainer.style.display = 'block';
     body.style.overflowY = 'auto';
   }
 });
@@ -30,7 +30,8 @@ searchCloseButton.addEventListener('click', function (e) {
   e.preventDefault();
   searchContainer.style.display = 'none';
   btn.classList.remove('is--active');
-  fixedAdvertiseContainer.style.display = 'block';
+  // fixedAdvertiseContainer.style.display = 'block';
+  body.style.overflowY = 'auto';
 });
 
 searchInput.oninput = function (e) {
